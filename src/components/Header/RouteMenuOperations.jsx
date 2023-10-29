a1fcc76fceb3e8ced7b2c5c70073e15cea60cc8a
Chinnawat-TT <chinnawat.sangrung@gmail.com>
2023-10-29 17:25:16 +0700

    page config little bit

--- src/components/Header/RouteMenuOperations.jsx
+++ src/components/Header/RouteMenuOperations.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+
 function RouteMenuOperations() {
+  const navigate = useNavigate()
   return (
     <div className="hidden sm:flex gap-4">
-      <button>หน้าหลัก</button>
+      <button onClick={()=> navigate("/")} >หน้าหลัก</button>
       <button>Category</button>
       <button>Category2</button>
     </div>

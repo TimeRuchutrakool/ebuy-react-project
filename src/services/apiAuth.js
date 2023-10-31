94b9eed91a9f02a8b0121e987122a12de09e530b
Chinnawat-TT <chinnawat.sangrung@gmail.com>
2023-10-31 11:53:26 +0700

    apiAuth getme login logout

---
+++ src/services/apiAuth.js
@@ -0,0 +1,24 @@
+import axios from "../config/axios"
+import { removeAccessToken } from "../utils/services"
+
+export const getCurrentUser = async (token) => {
+    if (token) {
+      const response = await axios.get("/auth/me");
+      return response.data;
+    }
+    return null;
+  };
+
+export const login = async (credential) =>{
+    const response = await axios.post("/auth/login",credential)
+    return response.data
+}
+
+export const signup = async (info) => {
+    const response = await axios.post(`/api/auth/signup`, info);
+    return response.data;
+};
+
+export const logout = () => {
+    removeAccessToken();
+};

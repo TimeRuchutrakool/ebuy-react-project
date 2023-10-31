bad24a881b5d6adca31726797a2d7acaea85893e
Chinnawat-TT <chinnawat.sangrung@gmail.com>
2023-10-31 14:30:07 +0700

    modal loginForm

--- src/features/user/LoginForm.jsx
+++ src/features/user/LoginForm.jsx
@@ -1,5 +1,34 @@
+import { useForm } from "react-hook-form";
 function LoginForm() {
-  return <div>Login</div>;
+  const { register,handleSubmit ,formState} = useForm();
+  const {errors} =formState
+  const onSubmit =(data)=> console.log(data)
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Sign in</h1>
+      <div>
+        <span>Email</span>
+        <input 
+        placeholder="Enter your email" 
+        type="email"
+        {...register("email", {required: "Incorrect. Please try again.", pattern: /^\S+@\S+$/i})}
+        />
+        <span>{errors?.email?.message}</span>
+      </div>
+      <div>
+        <span>Password</span>
+        <input 
+        placeholder="Enter your password" 
+        type="password"
+        {...register("password",{required :" invalid password "})}
+        />
+        <span>{errors?.password?.message}</span>
+      </div>
+      <button className=" bg-green-900 text-white">Sign in</button>
+      <hr />
+      <button className=" border">Sign in with Google</button>
+    </form>
+  );
 }
 
 export default LoginForm;

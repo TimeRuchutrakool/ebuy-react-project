ddc99160d3d56b8d17b0c8193d5e258c08f39a55
PhisitLothaka <tenphisit1998@gmail.com>
2023-11-01 22:02:57 +0700

    search to params

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import FilterOption from "../components/FilterOption";
 import ProductCard from "../features/product/ProductCard";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 export default function Search() {
+  const [allProduct, setAllProduct] = useState([]);
+  console.log("🚀 ~ file: Search.jsx:10 ~ Search ~ allProduct:", allProduct);
+  const { searchedTitle } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`/product/searchedTitle/${searchedTitle}`)
+      .then((res) => setAllProduct(res.data.data));
+  }, [searchedTitle]);
+
   return (
     <div className="px-36  min-h-[59vh]">
       <div className="text-center py-10 text-2xl">Showing product</div>
@@ -10,13 +24,15 @@ export default function Search() {
           <FilterOption />
         </div>
         <div className="w-4/5 grid grid-cols-3 place-items-center">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {allProduct.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              productImage={product.productImage}
+              avgRating={product.avgRating}
+            />
+          ))}
         </div>
       </div>
     </div>

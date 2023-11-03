6354a4673254a5317591bccdfe3aa43d183a5582
PhisitLothaka <tenphisit1998@gmail.com>
2023-11-03 16:36:16 +0700

    dalete console.log

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
-import  io  from "socket.io-client";
+import io from "socket.io-client";
 import { BACKEND_URL } from "./config/env";
 
-const socket = io.connect(BACKEND_URL,{reconnect:true});
+const socket = io.connect(BACKEND_URL, { reconnect: true });
 
 export default socket;

c930c08837f6eea84a75d33228380576b9ff428f
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-11-05 21:25:04 +0700

    cart v.2

--- src/features/cart/CartPurchase.jsx
+++ src/features/cart/CartPurchase.jsx
@@ -1,16 +1,21 @@
-export default function CartPurchase() {
+import { useNavigate } from "react-router-dom";
+
+export default function CartPurchase({ totalPrice }) {
+  const navigate = useNavigate();
   return (
-    // w-[610px] h-[200px]
-    <div className=" w-[610px]  p-3  border flex flex-col gap-2 rounded-md ">
-      <div className=" flex justify-between">
-        <p>item 2</p>
-        <p>2000 Bath</p>
+    <div className=" p-3  border flex flex-col gap-2 rounded-md ">
+      <div className=" flex justify-between font-light">
+        <p>Total</p>
+        <p>{totalPrice} ฿</p>
       </div>
       <div className=" flex justify-between">
-        <p>Subtotal</p>
-        <p>2000 Bath</p>
+        <p>Purchase</p>
+        <p>{totalPrice} ฿</p>
       </div>
-      <button className=" bg-[#1E4C2F] text-white rounded-xl p-2 mt-4">
+      <button
+        className=" bg-[#1E4C2F] text-white rounded-xl p-2 mt-4"
+        onClick={() => navigate("/order", { replace: true })}
+      >
         Go to checkout
       </button>
     </div>
---
+++ src/services/apiCart.js
@@ -0,0 +1,24 @@
+import axios from "../config/axios";
+
+export const getCart = async () => {
+  const res = await axios.get("/cart");
+
+  return res.data;
+};
+
+export const addCart = async (product) => {
+  const res = await axios.post("/cart/addCart", product);
+  return res.data;
+};
+
+export const updateAmount = async (method, cartItemId) => {
+  const res = await axios.patch(
+    `/cart/amount/?method=${method}&cartItemId=${cartItemId}`
+  );
+  return res.data;
+};
+
+export const deleteProductFromCart = async (cartItemId) => {
+  const res = await axios.delete(`/cart/${cartItemId}`);
+  return res.data;
+};

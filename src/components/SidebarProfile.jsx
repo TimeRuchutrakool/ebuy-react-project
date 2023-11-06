b19551bf9a539d8a758cb550fe3e1f888a979ae1
Nanthaphon <Nanthaphon909@gmail.com>
2023-11-06 17:18:29 +0700

    edit-sidebar-by-pete

--- src/components/SidebarProfile.jsx
+++ src/components/SidebarProfile.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import { AiOutlineRight } from "react-icons/ai";
 import EditProfile from "./EditProfile";
 import { FiEdit } from "react-icons/fi";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function SidebarProfile({ setMode }) {
+  const fileEl = useRef();
+
+  const onChangeImage = (e) => {
+    // console.log(e.target.files[0]);
+  };
+
   return (
-    <div className="flex flex-col  my-24 items-center relative  w-[300px]  border-t-2 border-r-2  h-full  ">
-      <div className="flex justify-end absolute top-[-2.5rem] items-end ">
+    <div className="flex flex-col my-24 items-center relative  w-[300px]  border-t-2 border-r-2  h-full">
+      <div
+        onClick={() => {
+          fileEl.current.click();
+        }}
+        className="flex justify-end absolute top-[-2.5rem] items-end cursor-pointer  "
+      >
         <img
           src="https://media.discordapp.net/attachments/1169121185053818954/1169273113473732718/gintoki_icon.jpeg?ex=6554cd72&is=65425872&hm=df872f430bbe5c701a5d3efbcf3efe1209b4c0a074957ae49eddd890b59f0375&=&width=1170&height=1170"
           alt=""
@@ -16,13 +29,20 @@ export default function SidebarProfile({ setMode }) {
           {" "}
           <FiEdit />
         </div>
+        <input
+          type="file"
+          className="hidden"
+          ref={fileEl}
+          onChange={onChangeImage}
+        />
       </div>
-      <div className="w-full h-full flex flex-col gap-4 my-10">
+
+      <div className="w-full h-full flex flex-col gap-4 bg-white pt-14">
         <div className="text-center">Gojo satoru</div>
-        <div className="text-center flex flex-col">
+        <div className="text-center flex flex-col h-full">
           <div
             onClick={() => setMode("EDIT")}
-            className="flex items-center px-8 py-4 cursor-pointer hover:bg-green-900 hover:text-white gap-4 justify-center border-t-2"
+            className="flex items-center px-8 py-4 cursor-pointer hover:bg-green-900 hover:text-white gap-4 justify-center border-t-2 mt-8"
           >
             <div>ข้อมูลส่วนตัว</div>
             <AiOutlineRight />

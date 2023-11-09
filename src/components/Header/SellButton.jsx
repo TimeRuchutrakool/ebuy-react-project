843ac56024998489e5004417bc9c5c234ce6b7be
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-11-09 21:01:53 +0700

    delete bid action on sell button

--- src/components/Header/SellButton.jsx
+++ src/components/Header/SellButton.jsx
@@ -1,5 +1,4 @@
 import { AiOutlinePlus } from "react-icons/ai";
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
 
 function SellButton() {
@@ -12,18 +11,6 @@ function SellButton() {
     <button
       className="px-4 py-1 bg-[#1E4C2F] text-[#ffffff] text-sm border rounded-full flex items-center gap-1"
       onClick={handleClick}
-=======
-import useModal from "../../hooks/useModal";
-
-function SellButton() {
-  const { dispatch: modal } = useModal();
-  return (
-    <button
-      className="px-4 py-1 bg-[#1E4C2F] text-[#ffffff] text-sm border rounded-full flex items-center gap-1"
-      onClick={() => {
-        modal({ type: "bid" });
-      }}
->>>>>>> feature/bid
     >
       <AiOutlinePlus />
       <span> ลงขาย</span>

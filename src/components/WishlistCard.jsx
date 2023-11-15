2e470e69a7491c36b1ecf23883fad7dbc22bef5e
PhisitLothaka <tenphisit1998@gmail.com>
2023-11-16 00:17:54 +0700

    wishlist successly

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -1,26 +1,47 @@
-import { AiOutlineHeart, AiFillStar } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
+import { MdFavorite } from "react-icons/md";
+import axios from "../config/axios";
 
-export default function WishlistCard() {
+export default function WishlistCard({
+  imageUrl,
+  name,
+  price,
+  product,
+  isWish,
+  setIsWish,
+}) {
+  const toggleWish = async (e) => {
+    e.preventDefault;
+    try {
+      await axios.post(`/wish/toggleWish/${product.id}`);
+      setIsWish(!isWish);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="w-fit flex flex-col gap-2">
+    <div className="w-[208px] flex flex-col gap-2 ">
       <div className="bg-[#F6F6F6] w-52 h-52 border  rounded-md flex justify-center items-center relative">
         <div className="w-8 rounded-full h-8 bg-white flex items-center justify-center absolute top-3 right-3">
-          <AiOutlineHeart className="" />
+          <button className="text-2xl" onClick={toggleWish}>
+            <MdFavorite className="text-red-500" />
+          </button>
         </div>
         <img
-          src="https://i.pinimg.com/564x/c9/89/3f/c9893fcccfd321ecfdb07b9037e38d62.jpg"
+          src={imageUrl}
           alt="productImage"
           className="w-40 h-40 object-cover overflow-hidden object-center"
         />
       </div>
-      <div className="flex justify-between">
-        <p className="font-extralight">เสื้อยืดมือสอง</p>
-        <p className="font-extralight text-[#1D9E34]">฿590</p>
+      <div className="flex  gap-2 justify-between">
+        <p className="font-extralight">{name}</p>
+        <p className="font-extralight  text-[#1D9E34]">฿590</p>
       </div>
       <p className="font-thin text-[#818B9C]">นันทพล นายาต</p>
       <span className="flex items-center gap-1">
         <AiFillStar className="text-[#FFA439]" />
-        <p className="font-light">test</p>
+        <p className="font-light">{price}</p>
       </span>
     </div>
   );

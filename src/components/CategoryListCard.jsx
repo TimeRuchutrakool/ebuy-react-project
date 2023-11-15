d6900ef2f2304c425b9c521697aafeb6afb82a97
Nanthaphon <Nanthaphon909@gmail.com>
2023-11-15 21:41:33 +0700

    editIconsCategory

--- src/components/CategoryListCard.jsx
+++ src/components/CategoryListCard.jsx
@@ -1,16 +1,27 @@
 import { useNavigate } from "react-router-dom";
-import { mockCategory } from "../mock/category";
 import CategoryCard from "./CategoryCard";
 import Heading from "./Heading";
+import { IoShirtSharp } from "react-icons/io5";
+import { PiPantsFill } from "react-icons/pi";
+import { GiConverseShoe } from "react-icons/gi";
+import { HiShoppingBag } from "react-icons/hi2";
+import { GiAmpleDress } from "react-icons/gi";
 
 function CategoryListCard() {
   const navigate = useNavigate();
+  const iconCategory = [
+    { icon: <PiPantsFill className="" />, name: "Pants" },
+    { icon: <IoShirtSharp />, name: "Shirt" },
+    { icon: <GiConverseShoe />, name: "shoes" },
+    { icon: <HiShoppingBag />, name: "Bag" },
+    { icon: <GiAmpleDress />, name: "dress" },
+  ];
 
   return (
     <div className="w-full flex flex-col items-center gap-10">
       <Heading big={true}>Shop By Category</Heading>
       <div className="flex justify-between gap-8">
-        {mockCategory.map((category, index) => (
+        {iconCategory.map((category, index) => (
           <CategoryCard
             category={category}
             key={index}
